8a1c6c94653532129c7562d4a0bbc94d3361f732
lj1995-computer-vision <61866546+lj1995-computer-vision@users.noreply.github.com>
2020-04-28 15:40:43 +0800

    Add files via upload

---
+++ DCNv2_pytorch1/_ext/dcn_v2_double/__init__.py
@@ -0,0 +1,38 @@
+# from torch.utils.ffi import _wrap_function
+from ._dcn_v2_double import lib as _lib, ffi as _ffi
+from functools import wraps
+import torch
+_torch_to_cffi = {}
+
+def _wrap_function(function, ffi):
+    @wraps(function)
+    def safe_call(*args, **kwargs):
+        args = tuple(ffi.cast(_torch_to_cffi.get(arg.type(), 'void') + '*', arg._cdata)
+                     if isinstance(arg, torch.Tensor) or torch.is_storage(arg)
+                     else arg
+                     for arg in args)
+        args = (function,) + args
+        result = torch._C._safe_call(*args, **kwargs)
+        if isinstance(result, ffi.CData):
+            typeof = ffi.typeof(result)
+            if typeof.kind == 'pointer':
+                cdata = int(ffi.cast('uintptr_t', result))
+                cname = typeof.item.cname
+                if cname in _cffi_to_torch:
+                    # TODO: Maybe there is a less janky way to eval
+                    # off of this
+                    return eval(_cffi_to_torch[cname])(cdata=cdata)
+        return result
+    return safe_call
+
+__all__ = []
+def _import_symbols(locals):
+    for symbol in dir(_lib):
+        fn = getattr(_lib, symbol)
+        if callable(fn):
+            locals[symbol] = _wrap_function(fn, _ffi)
+        else:
+            locals[symbol] = fn
+        __all__.append(symbol)
+
+_import_symbols(locals())
